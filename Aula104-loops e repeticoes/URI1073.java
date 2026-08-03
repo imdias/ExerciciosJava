52a1c60a149b1a79ad63c0aaabdc11633384999b
Igor Dias <igordias.md@gmail.com>
2026-08-03 06:04:07 -0300

    Exercicio URI1073.java

---
+++ Aula104-loops e repeticoes/URI1073.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class URI1073{
+	public static void main(String args[]){
+		Scanner teclado = new Scanner(System.in);
+		int N;
+		int numero;
+
+		N =teclado.nextInt();
+		for (numero = 2 ; numero <= N ; numero += 2 ) {
+			System.out.println(numero+"ˆ2 = "+ numero * numero);
+		}
+		
+
+	}
+}
