683312bab132cda53104dbe1366f516df0d33b5e
Igor Dias <igordias.md@gmail.com>
2026-08-10 20:55:32 -0300

    Exercicio URI1011.java

---
+++ Aula107-Matrizes/URI1011.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class URI1011{
+	public static void main(String args[]){
+		Scanner teclado = new Scanner(System.in);
+		double volumeEsfera;
+
+		double raio = teclado.nextFloat();
+
+		volumeEsfera = (4.0/3) * 3.14159 * Math.pow(raio, 3);
+
+		System.out.printf("Volume = %.3f\n", volumeEsfera);
+
+		teclado.close();
+	}
+}
